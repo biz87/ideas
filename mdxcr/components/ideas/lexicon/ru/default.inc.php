c77891850e0df7b840eb22796f486bf1c1d0ca3e
Nikolay Savin <info@megawebs.kz>
2018-09-30 16:05:52 +0600

    add lexicon

--- mdxcr/components/ideas/lexicon/ru/default.inc.php
+++ mdxcr/components/ideas/lexicon/ru/default.inc.php
@@ -1,34 +1,34 @@
 <?php
 include_once 'setting.inc.php';
 
-$_lang['ideas'] = 'ideas';
-$_lang['ideas_menu_desc'] = 'Пример расширения для разработки.';
-$_lang['ideas_intro_msg'] = 'Вы можете выделять сразу несколько предметов при помощи Shift или Ctrl.';
+$_lang['ideas'] = 'Ideas';
+$_lang['ideas_menu_desc'] = 'Идеи, вопросы, проблемы';
+$_lang['ideas_intro_msg'] = 'Вы можете выделять сразу несколько постов при помощи Shift или Ctrl.';
 
-$_lang['ideas_items'] = 'Предметы';
+$_lang['ideas_items'] = 'Посты';
 $_lang['ideas_item_id'] = 'Id';
 $_lang['ideas_item_name'] = 'Название';
 $_lang['ideas_item_description'] = 'Описание';
 $_lang['ideas_item_active'] = 'Активно';
 
-$_lang['ideas_item_create'] = 'Создать предмет';
-$_lang['ideas_item_update'] = 'Изменить Предмет';
-$_lang['ideas_item_enable'] = 'Включить Предмет';
-$_lang['ideas_items_enable'] = 'Включить Предметы';
-$_lang['ideas_item_disable'] = 'Отключить Предмет';
-$_lang['ideas_items_disable'] = 'Отключить Предметы';
-$_lang['ideas_item_remove'] = 'Удалить Предмет';
-$_lang['ideas_items_remove'] = 'Удалить Предметы';
-$_lang['ideas_item_remove_confirm'] = 'Вы уверены, что хотите удалить этот Предмет?';
-$_lang['ideas_items_remove_confirm'] = 'Вы уверены, что хотите удалить эти Предметы?';
+$_lang['ideas_item_create'] = 'Создать пост';
+$_lang['ideas_item_update'] = 'Изменить пост';
+$_lang['ideas_item_enable'] = 'Включить пост';
+$_lang['ideas_items_enable'] = 'Включить посты';
+$_lang['ideas_item_disable'] = 'Отключить пост';
+$_lang['ideas_items_disable'] = 'Отключить посты';
+$_lang['ideas_item_remove'] = 'Удалить пост';
+$_lang['ideas_items_remove'] = 'Удалить посты';
+$_lang['ideas_item_remove_confirm'] = 'Вы уверены, что хотите удалить этот пост?';
+$_lang['ideas_items_remove_confirm'] = 'Вы уверены, что хотите удалить эти посты?';
 $_lang['ideas_item_active'] = 'Включено';
 
-$_lang['ideas_item_err_name'] = 'Вы должны указать имя Предмета.';
-$_lang['ideas_item_err_ae'] = 'Предмет с таким именем уже существует.';
-$_lang['ideas_item_err_nf'] = 'Предмет не найден.';
-$_lang['ideas_item_err_ns'] = 'Предмет не указан.';
-$_lang['ideas_item_err_remove'] = 'Ошибка при удалении Предмета.';
-$_lang['ideas_item_err_save'] = 'Ошибка при сохранении Предмета.';
+$_lang['ideas_item_err_name'] = 'Вы должны указать имя поста.';
+$_lang['ideas_item_err_ae'] = 'пост с таким именем уже существует.';
+$_lang['ideas_item_err_nf'] = 'пост не найден.';
+$_lang['ideas_item_err_ns'] = 'пост не указан.';
+$_lang['ideas_item_err_remove'] = 'Ошибка при удалении поста.';
+$_lang['ideas_item_err_save'] = 'Ошибка при сохранении поста.';
 
 $_lang['ideas_grid_search'] = 'Поиск';
 $_lang['ideas_grid_actions'] = 'Действия';
