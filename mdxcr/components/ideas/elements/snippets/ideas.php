b92fca5b5f054430ada5cb3d6b79631bcf83db26
Nikolay Savin <info@megawebs.kz>
2018-09-30 18:06:05 +0600

    add modal css and js into snippet

--- mdxcr/components/ideas/elements/snippets/ideas.php
+++ mdxcr/components/ideas/elements/snippets/ideas.php
@@ -4,38 +4,43 @@
 /** @var ideas $ideas */
 $ideas = $modx->getService('ideas', 'ideas', MODX_CORE_PATH . 'components/ideas/model/', $scriptProperties);
 if (!$ideas) {
-    return 'Could not load ideas class!';
-}
-
-// Do your snippet code here. This demo grabs 5 items from our custom table.
-$tpl = $modx->getOption('tpl', $scriptProperties, 'Item');
-$sortby = $modx->getOption('sortby', $scriptProperties, 'name');
-$sortdir = $modx->getOption('sortbir', $scriptProperties, 'ASC');
-$limit = $modx->getOption('limit', $scriptProperties, 5);
-$outputSeparator = $modx->getOption('outputSeparator', $scriptProperties, "\n");
-$toPlaceholder = $modx->getOption('toPlaceholder', $scriptProperties, false);
-
-// Build query
-$c = $modx->newQuery('ideasItem');
-$c->sortby($sortby, $sortdir);
-$c->where(['active' => 1]);
-$c->limit($limit);
-$items = $modx->getIterator('ideasItem', $c);
-
-// Iterate through items
-$list = [];
-/** @var ideasItem $item */
-foreach ($items as $item) {
-    $list[] = $modx->getChunk($tpl, $item->toArray());
-}
-
-// Output
-$output = implode($outputSeparator, $list);
-if (!empty($toPlaceholder)) {
-    // If using a placeholder, output nothing and set output to specified placeholder
-    $modx->setPlaceholder($toPlaceholder, $output);
-
+    $modx->log(modX::LOG_LEVEL_ERROR, 'Could not load ideas class!');
     return '';
 }
-// By default just return output
-return $output;
+
+$modx->regClientCSS(MODX_ASSETS_PATH . 'components/ideas/css/jquery.modal.min.css');
+$modx->regClientScript(MODX_ASSETS_PATH . 'components/ideas/js/jquery.modal.min.js');
+
+//
+//// Do your snippet code here. This demo grabs 5 items from our custom table.
+//$tpl = $modx->getOption('tpl', $scriptProperties, 'Item');
+//$sortby = $modx->getOption('sortby', $scriptProperties, 'name');
+//$sortdir = $modx->getOption('sortbir', $scriptProperties, 'ASC');
+//$limit = $modx->getOption('limit', $scriptProperties, 5);
+//$outputSeparator = $modx->getOption('outputSeparator', $scriptProperties, "\n");
+//$toPlaceholder = $modx->getOption('toPlaceholder', $scriptProperties, false);
+//
+//// Build query
+//$c = $modx->newQuery('ideasItem');
+//$c->sortby($sortby, $sortdir);
+//$c->where(['active' => 1]);
+//$c->limit($limit);
+//$items = $modx->getIterator('ideasItem', $c);
+//
+//// Iterate through items
+//$list = [];
+///** @var ideasItem $item */
+//foreach ($items as $item) {
+//    $list[] = $modx->getChunk($tpl, $item->toArray());
+//}
+//
+//// Output
+//$output = implode($outputSeparator, $list);
+//if (!empty($toPlaceholder)) {
+//    // If using a placeholder, output nothing and set output to specified placeholder
+//    $modx->setPlaceholder($toPlaceholder, $output);
+//
+//    return '';
+//}
+//// By default just return output
+//return $output;
