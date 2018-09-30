80eb1c58bbcd659ed9ff638dab015ec19961de1b
Nikolay Savin <info@megawebs.kz>
2018-09-30 18:58:09 +0600

    change snippet. Add getCollection

--- mdxcr/components/ideas/elements/snippets/ideas.php
+++ mdxcr/components/ideas/elements/snippets/ideas.php
@@ -8,44 +8,37 @@ if (!$ideas) {
     return '';
 }
 
-$allow_jquery_modal = $modx->getOption('allow_jquery-modal', null, true);
+$allow_jquery_modal = $modx->getOption('ideas_allow_jquery_modal', null, true);
 if($allow_jquery_modal){
     $modx->regClientCSS(MODX_ASSETS_URL . 'components/ideas/css/jquery.modal.min.css');
     $modx->regClientScript(MODX_ASSETS_URL. 'components/ideas/js/jquery.modal.min.js');
 }
 
+$pdoFetch = $modx->getService('pdoFetch');
+
+$ideasPosts = $pdoFetch->getCollection(
+    'ideasPosts',
+    array('active' => 1),
+    array(
+        'sortby' => 'createdon',
+        'sortdir' => 'asc',
+        'innerJoin' => array(
+            'Status' => array(
+                'class' => 'ideasStatus',
+                'on' => 'ideasPosts.status = Status.id'
+            ),
+            'Type' => array(
+                'class' => 'ideasType',
+                'on' => 'ideasPosts.type = Type.id'
+            )
+        ),
+        'select' => array(
+            'ideasPosts' => 'name, description',
+            'Status' => 'Status.name as status_name',
+            'Type' => 'Type.name as type_name'
+        ),
+        'limit' => 20
+    )
+);
 
 
-//
-//// Do your snippet code here. This demo grabs 5 items from our custom table.
-//$tpl = $modx->getOption('tpl', $scriptProperties, 'Item');
-//$sortby = $modx->getOption('sortby', $scriptProperties, 'name');
-//$sortdir = $modx->getOption('sortbir', $scriptProperties, 'ASC');
-//$limit = $modx->getOption('limit', $scriptProperties, 5);
-//$outputSeparator = $modx->getOption('outputSeparator', $scriptProperties, "\n");
-//$toPlaceholder = $modx->getOption('toPlaceholder', $scriptProperties, false);
-//
-//// Build query
-//$c = $modx->newQuery('ideasItem');
-//$c->sortby($sortby, $sortdir);
-//$c->where(['active' => 1]);
-//$c->limit($limit);
-//$items = $modx->getIterator('ideasItem', $c);
-//
-//// Iterate through items
-//$list = [];
-///** @var ideasItem $item */
-//foreach ($items as $item) {
-//    $list[] = $modx->getChunk($tpl, $item->toArray());
-//}
-//
-//// Output
-//$output = implode($outputSeparator, $list);
-//if (!empty($toPlaceholder)) {
-//    // If using a placeholder, output nothing and set output to specified placeholder
-//    $modx->setPlaceholder($toPlaceholder, $output);
-//
-//    return '';
-//}
-//// By default just return output
-//return $output;
