edbcbc03448c6708f51d9c97d44892ea08701a62
Nikolay Savin <info@megawebs.kz>
2018-11-04 14:26:02 +0600

    new settings

--- _build/elements/settings.php
+++ _build/elements/settings.php
@@ -6,4 +6,30 @@ return [
         'value' => true,
         'area' => 'ideas_main',
     ],
+    'allow_vote_anonimus' => [
+        'xtype' => 'combo-boolean',
+        'value' => true,
+        'area' => 'ideas_main',
+    ],
+    'first_status' => [
+        'xtype' => 'numberfield',
+        'value' => 1,
+        'area' => 'ideas_main',
+    ],
+    'allow_new_ideas_anonimus' => [
+        'xtype' => 'combo-boolean',
+        'value' => false,
+        'area' => 'ideas_main',
+    ],
+    'allow_published' => [
+        'xtype' => 'combo-boolean',
+        'value' => false,
+        'area' => 'ideas_main',
+    ],
+    'manager_email' => [
+        'xtype' => 'textfield',
+        'value' => '',
+        'area' => 'ideas_main',
+    ],
+
 ];
--- mdxcr/components/ideas/lexicon/ru/setting.inc.php
+++ mdxcr/components/ideas/lexicon/ru/setting.inc.php
@@ -4,3 +4,19 @@ $_lang['area_ideas_main'] = 'Основные';
 
 $_lang['setting_ideas_allow_jquery_modal'] = 'Разрешить jquery modal';
 $_lang['setting_ideas_allow_jquery_modal_desc'] = 'Разрешить сниппету подключать библиотеку jquery modal для вызова модального окна';
+
+$_lang['setting_ideas_allow_vote_anonimus'] = 'Разрешить голосовать анонимам';
+$_lang['setting_ideas_allow_vote_anonimus_desc'] = 'По умолчанию разрешаем голосовать за идеи любым пользователям';
+
+$_lang['setting_ideas_first_status'] = 'Статус публикации новой идеи';
+$_lang['setting_ideas_first_status_desc'] = 'ID статуса, с которым будет публиковаться новая идея. ';
+
+$_lang['setting_ideas_allow_new_ideas_anonimus'] = 'Разрешить предлагать идеи анонимам';
+$_lang['setting_ideas_allow_new_ideas_anonimus_desc'] = 'По умолчанию разрешаем публиковать свои идеи только зарегистрированным пользователям';
+
+$_lang['setting_ideas_allow_published'] = 'Публиковать идеи сразу без модерации';
+$_lang['setting_ideas_allow_published_desc'] = 'По умолчанию идея должна пройти предварительную модерацию';
+
+
+$_lang['setting_ideas_manager_email'] = 'Email менеджера';
+$_lang['setting_ideas_manager_email_desc'] = 'Куда прислать уведомление о новой идее?  Можно несколько адресов, через запятую';
